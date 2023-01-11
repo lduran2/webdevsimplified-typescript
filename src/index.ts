85181702534519b4c64b3bca0b10cffd324740f4
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-01-10 20:03:08 -0500

    [wds/ts] fetching the elements in TS

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 'use strict';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
-console.log(`Hi ${uuidv4()}`);
+// console.log(`Hi ${uuidv4()}`);
+
+const todos = document.querySelector('#todos');
+const form = document.forms.newTask as HTMLFormElement;
+const title = form.taskTitle as HTMLInputElement;
+
+console.log([todos, form, [title.name, title.type, title.value]]);
